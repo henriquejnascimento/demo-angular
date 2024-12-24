6a048350ca10af8c75f18eca415d68830897e117
Henrique Nascimento <contato@henriquenascimento.com>
2024-12-24 16:53:40 -0300

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'crud',
+    component: ProductComponent
+  },
+  {
+    path: '',
+    redirectTo: '/crud',
+    pathMatch: 'full'
+  }
+];
