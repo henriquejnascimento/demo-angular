20d76f7d2a956f7a1806f53a5c24c637f44bf2f8
Henrique Nascimento <contato@henriquenascimento.com>
2025-01-05 23:55:37 -0300

    Added BaseField

---
+++ src/app/models/base-field.model.ts
@@ -0,0 +1,15 @@
+import { ValidatorFn } from '@angular/forms';
+import { FormFieldType } from '../enums/form-field-type';
+
+export class BaseField {
+  type!: FormFieldType;
+  name!: string;
+  label!: string;
+  validators?: ValidatorFn[];
+  placeholder? :string;
+  value? :string;    
+  disabled?: boolean;
+  mask? :string;
+  class? :string;
+  style? :string;
+}
