ce939577c9de978539e3ad62495bd971b6e5ef1d
Henrique Nascimento <contato@henriquenascimento.com>
2025-01-07 18:21:41 -0300

    Fix import

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PaginatedResponse } from '../components/models/paginated-response.model';
+import { PaginatedResponse } from '..//models/paginated-response.model';
 
 @Injectable({
   providedIn: 'root'
